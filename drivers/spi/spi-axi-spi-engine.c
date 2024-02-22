1de2499560ed7fe1392c03c6a2da3f760f333770
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-02-22 19:37:26 -0300

    spi-engine: Fix max CS
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/spi/spi-axi-spi-engine.c
+++ drivers/spi/spi-axi-spi-engine.c
@@ -651,7 +651,7 @@ static int spi_engine_probe(struct platform_device *pdev)
 	host->transfer_one_message = spi_engine_transfer_one_message;
 	host->optimize_message = spi_engine_optimize_message;
 	host->unoptimize_message = spi_engine_unoptimize_message;
-	host->num_chipselect = 8;
+	host->num_chipselect = 4;
 
 	if (host->max_speed_hz == 0)
 		return dev_err_probe(&pdev->dev, -EINVAL, "spi_clk rate is 0");
