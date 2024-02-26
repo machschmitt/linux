8d15685a05bc0429ba57bdd5593a7a0120d7792d
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-02-26 16:53:48 -0300

    ad4000: high_z_en -> high_impedance_en
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4000.c
+++ drivers/iio/adc/ad4000.c
@@ -418,7 +418,7 @@ static IIO_DEVICE_ATTR(span_compression_en, S_IRUGO | S_IWUSR,
 		       ad4000_show, ad4000_store,
 		       AD4000_SPANC);
 
-static IIO_DEVICE_ATTR(high_z_en, S_IRUGO | S_IWUSR,
+static IIO_DEVICE_ATTR(high_impedance_en, S_IRUGO | S_IWUSR,
 		       ad4000_show, ad4000_store,
 		       AD4000_HIGHZ);
 
@@ -430,7 +430,7 @@ static IIO_DEVICE_ATTR(turbo_en, S_IRUGO | S_IWUSR,
 static struct attribute *ad4000_attributes[] = {
 	&iio_dev_attr_status_bits_en.dev_attr.attr,
 	&iio_dev_attr_span_compression_en.dev_attr.attr,
-	&iio_dev_attr_high_z_en.dev_attr.attr,
+	&iio_dev_attr_high_impedance_en.dev_attr.attr,
 	&iio_dev_attr_turbo_en.dev_attr.attr,
 	NULL
 };
