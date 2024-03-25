63d2bfff6d9e01b17208cb5140245eb8644f9a42
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-03-25 18:00:18 -0300

    ad4000: Remove include of, trigger; add include mod_devicetable
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4000.c
+++ drivers/iio/adc/ad4000.c
@@ -12,7 +12,7 @@
 #include <linux/kernel.h>
 #include <linux/math.h>
 #include <linux/module.h>
-#include <linux/of.h>
+#include <linux/mod_devicetable.h>
 #include <linux/gpio/consumer.h>
 #include <linux/regulator/consumer.h>
 #include <linux/spi/spi.h>
@@ -21,7 +21,6 @@
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 #include <linux/iio/buffer.h>
-#include <linux/iio/trigger.h>
 #include <linux/iio/triggered_buffer.h>
 #include <linux/iio/trigger_consumer.h>
 
