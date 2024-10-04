7be9ba45e69f9a3360e219b0ab9ed4468f669fd4
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-10-04 15:10:26 -0300

    iio: adc: ad4170: Update copyright year
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4170.c
+++ drivers/iio/adc/ad4170.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2023 Analog Devices, Inc.
+ * Copyright (C) 2024 Analog Devices, Inc.
  * Author: Ana-Maria Cusco <ana-maria.cusco@analog.com>
  */
 
