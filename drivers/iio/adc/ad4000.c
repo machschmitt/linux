c05c08cbb3fd080217a25cdd861a9ad26931652f
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-10-28 17:05:58 -0300

    iio: adc: ad4000: add offload flag for ad7685 chan_spec
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4000.c
+++ drivers/iio/adc/ad4000.c
@@ -326,7 +326,7 @@ static const struct ad4000_chip_info adaq4003_chip_info = {
 
 static const struct ad4000_chip_info ad7685_chip_info = {
 	.dev_name = "ad7685",
-	.chan_spec = AD4000_PSEUDO_DIFF_CHANNELS('u', 16, 0),
+	.chan_spec = AD4000_PSEUDO_DIFF_CHANNELS('u', 16, 0, 0),
 	.max_rate_hz  = 250000,
 };
 
