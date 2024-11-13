4614b2c05f714e3ba5c6e7aaba9b3977fff49b8b
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-11-13 16:59:15 -0300

    iio: adc: ad4000: Add max_rate_hz to AD7691
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4000.c
+++ drivers/iio/adc/ad4000.c
@@ -338,6 +338,7 @@ static const struct ad4000_chip_info ad7687_chip_info = {
 static const struct ad4000_chip_info ad7691_chip_info = {
 	.dev_name = "ad7691",
 	.chan_spec = AD4000_DIFF_CHANNELS('s', 18, 0),
+	.max_rate_hz  = 250000,
 };
 
 struct ad4000_state {
