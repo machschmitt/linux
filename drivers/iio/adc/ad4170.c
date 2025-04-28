c7ea39ead1678de9b3337400fa764d269b1b4d8d
Marcelo Schmitt <marcelo.schmitt@analog.com>
2025-04-27 23:49:24 -0300

    iio: adc: ad4170: Setup thermocouple the same way RTDs are setup
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4170.c
+++ drivers/iio/adc/ad4170.c
@@ -2091,8 +2091,7 @@ static int ad4170_parse_external_sensor(struct ad4170_state *st,
 					    reg_val);
 		}
 	}
-	if (s_type == AD4170_WEIGH_SCALE_SENSOR ||
-	    s_type == AD4170_THERMOCOUPLE_SENSOR) {
+	if (s_type == AD4170_WEIGH_SCALE_SENSOR) {
 		ret = ad4170_setup_bridge(st, child, setup, exc_pins,
 					  num_exc_pins, exc_cur, ac_excited);
 	} else {
