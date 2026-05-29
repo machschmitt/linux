61aa8ab473a727623499e59e403a5dc690e64b5d
Marcelo Schmitt <marcelo.schmitt@analog.com>
2026-05-29 17:04:02 -0300

    spi: spi-axi-spi-egnine: Keep support for features introduced after v1
    Keep support for features introduces after SPI Engine v1.0 for newer
    versions of SPI Engine.
    
    Fixes: <...> ""
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/spi/spi-axi-spi-engine.c
+++ drivers/spi/spi-axi-spi-engine.c
@@ -1227,11 +1227,13 @@ static int spi_engine_probe(struct platform_device *pdev)
 
 	/* Some features depend of the IP core version. */
 	if (ADI_AXI_PCORE_VER_MAJOR(version) >= 1) {
-		if (ADI_AXI_PCORE_VER_MINOR(version) >= 2) {
+		if (ADI_AXI_PCORE_VER_MAJOR(version) >= 2 ||
+		    ADI_AXI_PCORE_VER_MINOR(version) >= 2) {
 			host->mode_bits |= SPI_CS_HIGH;
 			host->setup = spi_engine_setup;
 		}
-		if (ADI_AXI_PCORE_VER_MINOR(version) >= 3)
+		if (ADI_AXI_PCORE_VER_MAJOR(version) >= 2 ||
+		    ADI_AXI_PCORE_VER_MINOR(version) >= 3)
 			host->mode_bits |= SPI_MOSI_IDLE_LOW | SPI_MOSI_IDLE_HIGH;
 	}
 	if (ADI_AXI_PCORE_VER_MAJOR(version) >= 2)
