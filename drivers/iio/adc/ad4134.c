4b06e9511f0e0c7ab6858232dec4e1b9c0b66fb5
Marcelo Schmitt <marcelo.schmitt@analog.com>
2026-07-14 16:59:29 -0300

    iio: adc: ad4134: Update include list to comply with IWYU principles
    Add includes that were missing (according to include-what-you-use tool)
    to comply with include what is used principles. Namely, add time64.h for
    USEC_PER_SEC.
    
    Remove time.h which is covered by time64.h and iio/types.h which is
    covered by iio/iio.h.
    
    While updating the list, organize it to follow current IIO include list
    convention.
    
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4134.c
+++ drivers/iio/adc/ad4134.c
@@ -14,17 +14,17 @@
 #include <linux/err.h>
 #include <linux/export.h>
 #include <linux/gpio/consumer.h>
-#include <linux/iio/iio.h>
-#include <linux/iio/types.h>
 #include <linux/module.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
 #include <linux/reset.h>
 #include <linux/spi/spi.h>
-#include <linux/time.h>
+#include <linux/time64.h>
 #include <linux/types.h>
 #include <linux/unaligned.h>
 #include <linux/units.h>
+
+#include <linux/iio/iio.h>
 
 #define AD4134_RESET_TIME_US			(10 * USEC_PER_SEC)
 
