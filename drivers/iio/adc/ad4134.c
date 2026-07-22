5dcdd784786f2893b6c6eb2fd3fc6b848b5ea25a
Marcelo Schmitt <marcelo.schmitt@analog.com>
2026-07-22 15:30:48 -0300

    iio: adc: ad4134: [DONT SEND] Per channel sampling frequency selection
    iio: adc: ad4134: [SQUASH] fix fallthrough in CHAN_ODR_SEL set
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad4134.c
+++ drivers/iio/adc/ad4134.c
@@ -78,6 +78,9 @@
 #define AD4134_PW_DOWN_CTRL_REG			0x13
 #define AD4134_DEVICE_STATUS_REG		0x15
 #define AD4134_ODR_VAL_INT_LSB_REG		0x16
+#define AD4134_CHAN_ODR_SEL_REG			0x1D
+#define AD4134_CHAN_SEL_MASK(ch)		(GENMASK(1, 0) << 2 * (ch))
+#define AD4134_CHAN_ODR_SEL_OPTIONS		4
 #define AD4134_CHAN_DIG_FILTER_SEL_REG		0x1E
 #define AD4134_CHAN_DIG_FILTER_SEL_CH_MASK(ch)	(GENMASK(1, 0) << 2 * (ch))
 
@@ -240,6 +243,7 @@ struct ad4134_state {
 	struct pwm_waveform odr_wf;
 	struct pwm_device *odr_pwm;
 	unsigned int odr_hz;
+	int odr_rate_tbl[AD4134_CHAN_ODR_SEL_OPTIONS];
 	bool crc_en;
 	/*
 	 * DMA (thus cache coherency maintenance) requires the transfer buffers
@@ -526,6 +530,12 @@ static const struct regmap_config ad4134_regmap_config = {
 	.max_register = AD4134_CH_VREG(ARRAY_SIZE(ad4134_chan_set)),
 };
 
+static void ad4134_fill_odr_tbl(struct ad4134_state *st)
+{
+	for (unsigned int i = 0; i < AD4134_CHAN_ODR_SEL_OPTIONS; i++)
+		st->odr_rate_tbl[i] = st->odr_hz >> i; /* odr_hz / 2^i */
+}
+
 static int ad4134_update_conversion_rate(struct ad4134_state *st,
 					 unsigned int freq_Hz)
 {
@@ -607,6 +617,8 @@ static int ad4134_update_conversion_rate(struct ad4134_state *st,
 	st->offload_trigger_config = config;
 	st->odr_wf = odr_wf;
 	st->odr_hz = DIV_ROUND_UP_ULL(NSEC_PER_SEC, odr_wf.period_length_ns);
+
+	ad4134_fill_odr_tbl(st);
 
 	return 0;
 }
@@ -703,6 +715,7 @@ static int ad4134_read_raw(struct iio_dev *indio_dev,
 		return -EINVAL;
 	}
 }
+
 static int ad4134_read_avail(struct iio_dev *indio_dev,
 			     struct iio_chan_spec const *chan,
 			     const int **vals, int *type, int *length,
